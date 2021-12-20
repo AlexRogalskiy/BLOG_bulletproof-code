1a09b25574cb14e3ce680ed3f00d113a91fab052
Wilder Dev <bulletproofcode@outlook.com>
2021-12-20 17:10:08 -0600

    ADD: Thank You Template

--- pages/thank-you/[slug].js
+++ pages/thank-you/[slug].js
@@ -1,10 +1,22 @@
 import MainLayout from "../../layouts/MainLayout";
 import allOffers from "../../data/offers.db.json";
+import Image from "next/image";
 
 const ThankYouTemplate = ({ optInItem }) => {
   return (
     <MainLayout>
-      <h1 className="">Thank You! {optInItem.name}</h1>
+      <h1 className="text-4xl text-center leading-snug">
+        Thank You for Downloding the{" "}
+        <span className="font-black block">{optInItem.name}</span>
+      </h1>
+      <div className="relative w-96 h-96 mx-auto my-12">
+        <Image
+          src={optInItem.image}
+          alt={optInItem.name}
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
     </MainLayout>
   );
 };
