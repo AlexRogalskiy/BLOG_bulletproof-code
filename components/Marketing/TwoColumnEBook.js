f69945b52bef86d89e51cccfe36bc7bfed18fac6
Wilder Dev <bulletproofcode@outlook.com>
2021-12-27 08:20:43 -0600

    Landing OptIn Layout

--- components/Marketing/TwoColumnEBook.js
+++ components/Marketing/TwoColumnEBook.js
@@ -1,7 +1,64 @@
+import { ArrowRightIcon } from "@heroicons/react/outline";
+import { CheckCircleIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { v4 } from "uuid";
+
 const TwoColumnEBook = ({ eBookDetails }) => {
-  const { title, image, ctaCopty, wordCount, mainFeatures, optInLink } =
+  const { title, image, ctaCopy, wordCount, mainFeatures, optInLink } =
     eBookDetails;
-  return <div>Enter</div>;
+  return (
+    <article className="max-w-6xl mx-auto">
+      {/*  Top Section - Title & SubTitle */}
+      <section className="text-center space-y-12 mt-12 mb-16">
+        <h1 className="text-3xl font-bold text-red-700">{title}</h1>
+        <h2 className=" text-red-700 font-medium italic text-2xl">
+          No BS. No false promises. Just a step-by-step system that actually
+          works.
+        </h2>
+      </section>
+
+      <section className="flex justify-between">
+        {/* Left Section - Main Features */}
+        <section className="w-5/12">
+          <h3 className="">
+            Download my{" "}
+            <span className="font-bold">
+              free {wordCount}-word Bulletproof Guide to Becoming a PAID
+              Front-End Developer,
+            </span>{" "}
+            and I&apos;ll show you:
+          </h3>
+          <ul className="">
+            {mainFeatures.map((feature) => (
+              <li className="flex" key={v4()}>
+                <CheckCircleIcon className="h-12 w-12 text-green-500" />
+                <p className="">
+                  <span className="font-bold">{feature.feature}</span>
+                  {feature.description}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <ArrowRightIcon className="h-12 w-12" />
+        </section>
+
+        {/* Right Section - Image and CTA */}
+        <section className="w-5/12">
+          <div className="relative h-[450px] w-[450px] mx-auto">
+            <Image
+              src={image}
+              alt={title + " Book Cover"}
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+          <button className="bg-red-700 rounded mt-16 w-full py-4 px-8 text-white font-bold text-2xl uppercase hover:bg-red-800 transition-colors">
+            {ctaCopy}
+          </button>
+        </section>
+      </section>
+    </article>
+  );
 };
 
 export default TwoColumnEBook;
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,24 @@ const BPFrontEndGuide = {
   title: "Bulletproof Guide to Becoming a PAID Front-End Developer",
   image: "/static/images/bp-front-end/Book_Cover.png",
   wordCount: 16511,
-  optInLink: "",
-  mainFeatures: [],
+  optInLink: "/opt-in/bulletproof-guide-to-becoming-a-paid-front-end-developer",
+  mainFeatures: [
+    {
+      feature: "Strategically integrate action points in your business",
+      description:
+        "So we can hit the ground running, we will be dynamically investing every ballpark figure in our space.",
+    },
+    {
+      feature: "Virtually grow capabilities in your business",
+      description:
+        "Learn seamless best practices are becoming unparalleled prince2 practitioner experts",
+    },
+    {
+      feature: "Intelligently reuse step-changes in your business",
+      description:
+        "Touching base about engineering emerging markets will make us leaders in the holistic visibility industry",
+    },
+  ],
   ctaCopy: "Download my Free Guide!",
 };
 
