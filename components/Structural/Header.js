bd9310938e81a57f9d4bc8ca71e4886067e2d907
Wilder Dev <bulletproofcode@outlook.com>
2021-12-28 13:06:27 -0600

    ADD: Rainbow Border

--- components/Structural/Header.js
+++ components/Structural/Header.js
@@ -68,7 +68,7 @@ const moreResources = [
 const Header = () => {
   return (
     <Popover className="sticky top-0 bg-white z-50">
-      <div className="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-4 lg:space-x-10">
+      <div className="flex justify-between items-center px-4 py-4 sm:px-6 md:justify-start md:space-x-4 lg:space-x-10">
         <div className="flex md:hidden xl:flex justify-start lg:w-0 lg:flex-1">
           <Link href="/">
             <a>
